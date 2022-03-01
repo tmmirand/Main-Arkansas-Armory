4c81e668b997a65eb92aa3c7f6d5d8abfaa3ca3f
tmmirand <tmmirand@WCOB-LAB-037>
2022-03-01 12:09:01 -0600

    Add project files.

---
+++ Arkansas Armory/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿
+namespace Arkansas_Armory
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCreate_Account = new System.Windows.Forms.Button();
+            this.cboAccount = new System.Windows.Forms.ComboBox();
+            this.txtFirst_Name = new System.Windows.Forms.TextBox();
+            this.txtLast_Name = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Font = new System.Drawing.Font("HelveticaNeueforSAS", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(243, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(347, 47);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Create an Account";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Font = new System.Drawing.Font("HelveticaNeueforSAS", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(62, 139);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(155, 34);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "First Name";
+            // 
+            // btnCreate_Account
+            // 
+            this.btnCreate_Account.Location = new System.Drawing.Point(355, 390);
+            this.btnCreate_Account.Name = "btnCreate_Account";
+            this.btnCreate_Account.Size = new System.Drawing.Size(99, 48);
+            this.btnCreate_Account.TabIndex = 5;
+            this.btnCreate_Account.Text = "Create";
+            this.btnCreate_Account.UseVisualStyleBackColor = true;
+            this.btnCreate_Account.Click += new System.EventHandler(this.btnCreate_Account_Click);
+            // 
+            // cboAccount
+            // 
+            this.cboAccount.FormattingEnabled = true;
+            this.cboAccount.Location = new System.Drawing.Point(245, 80);
+            this.cboAccount.Name = "cboAccount";
+            this.cboAccount.Size = new System.Drawing.Size(345, 21);
+            this.cboAccount.TabIndex = 6;
+            this.cboAccount.SelectedIndexChanged += new System.EventHandler(this.cboAccount_SelectedIndexChanged);
+            // 
+            // txtFirst_Name
+            // 
+            this.txtFirst_Name.Location = new System.Drawing.Point(245, 139);
+            this.txtFirst_Name.Name = "txtFirst_Name";
+            this.txtFirst_Name.Size = new System.Drawing.Size(345, 20);
+            this.txtFirst_Name.TabIndex = 7;
+            // 
+            // txtLast_Name
+            // 
+            this.txtLast_Name.Location = new System.Drawing.Point(245, 203);
+            this.txtLast_Name.Name = "txtLast_Name";
+            this.txtLast_Name.Size = new System.Drawing.Size(345, 20);
+            this.txtLast_Name.TabIndex = 8;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(245, 271);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(345, 20);
+            this.txtEmail.TabIndex = 9;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(245, 332);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(345, 20);
+            this.txtPassword.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Font = new System.Drawing.Font("HelveticaNeueforSAS", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(65, 203);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(152, 34);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Last Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label4.Font = new System.Drawing.Font("HelveticaNeueforSAS", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(65, 271);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 34);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Email";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label5.Font = new System.Drawing.Font("HelveticaNeueforSAS", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(65, 332);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(140, 34);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Password";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkRed;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtLast_Name);
+            this.Controls.Add(this.txtFirst_Name);
+            this.Controls.Add(this.cboAccount);
+            this.Controls.Add(this.btnCreate_Account);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCreate_Account;
+        private System.Windows.Forms.ComboBox cboAccount;
+        private System.Windows.Forms.TextBox txtFirst_Name;
+        private System.Windows.Forms.TextBox txtLast_Name;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Arkansas Armory/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Arkansas_Armory
+{
+    public partial class Form1 : Form
+    {
+        /////////////////////////////////////////////////////
+        SqlConnection connection;
+        SqlCommand command;
+        SqlDataReader datareader;
+        string connectionstring = "Data Source=essql1.walton.uark.edu;Initial Catalog=PROJECTS2241;User ID=PROJECTS2241;Password=CN74kyu$";
+        /////////////////////////////////////////////////////
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cboAccount_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            /////////////////////////////////////////////////////
+            try
+            {
+                if (cboAccount.SelectedIndex > -1)
+                {
+                    connection = new SqlConnection(connectionstring);
+                    connection.Open();
+                    string sql = "SELECT First_Name, Last_Name, Email, Password FROM Account WHERE First_Name = '" + cboAccount.SelectedItem.ToString() + "'";
+                    command = new SqlCommand(sql, connection);
+                    datareader = command.ExecuteReader();
+                    while (datareader.Read())
+                    {
+                        txtFirst_Name.Text = datareader[0].ToString();
+                        txtLast_Name.Text = datareader[1].ToString();
+                        txtEmail.Text = datareader[2].ToString();
+                        txtPassword.Text = datareader[3].ToString();
+                    }
+                    connection.Close();
+                    command.Dispose();
+                    datareader.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error" + ex);
+            }
+            /////////////////////////////////////////////////////
+        }
+
+        private void btnCreate_Account_Click(object sender, EventArgs e)
+        {
+            /////////////////////////////////////////////////////
+            try
+            {
+                connection = new SqlConnection(connectionstring);
+                connection.Open();
+                int answer;
+                string sql = "INSERT INTO Account VALUES (@Fname, @Lname, @email, @password)";
+                command = new SqlCommand(sql, connection);
+                command.Parameters.AddWithValue("@Fname", txtFirst_Name.Text);
+                command.Parameters.AddWithValue("@Lname", txtLast_Name.Text);
+                command.Parameters.AddWithValue("@email", txtEmail.Text);
+                command.Parameters.AddWithValue("@password", txtPassword.Text);
+
+                answer = command.ExecuteNonQuery();
+
+                connection.Close();
+                command.Dispose();
+                MessageBox.Show("You have successfully entered " + answer + " into the database");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error! You need to solve: " + ex);
+            }
+            /////////////////////////////////////////////////////
+        }
+    }
+}
