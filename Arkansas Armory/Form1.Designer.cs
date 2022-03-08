ce9d5ec09acd8410d531117f14d78b49b29e6c42
jgdodson <jgdodson@uark.edu>
2022-03-08 11:09:55 -0600

    Added panel for create account

--- Arkansas Armory/Form1.Designer.cs
+++ Arkansas Armory/Form1.Designer.cs
@@ -40,6 +40,8 @@ namespace Arkansas_Armory
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -47,7 +49,7 @@ namespace Arkansas_Armory
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.label1.Font = new System.Drawing.Font("HelveticaNeueforSAS", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(243, 9);
+            this.label1.Location = new System.Drawing.Point(194, 15);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(347, 47);
             this.label1.TabIndex = 0;
@@ -58,7 +60,7 @@ namespace Arkansas_Armory
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.label2.Font = new System.Drawing.Font("HelveticaNeueforSAS", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(62, 139);
+            this.label2.Location = new System.Drawing.Point(13, 145);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(155, 34);
             this.label2.TabIndex = 1;
@@ -66,7 +68,7 @@ namespace Arkansas_Armory
             // 
             // btnCreate_Account
             // 
-            this.btnCreate_Account.Location = new System.Drawing.Point(355, 390);
+            this.btnCreate_Account.Location = new System.Drawing.Point(365, 487);
             this.btnCreate_Account.Name = "btnCreate_Account";
             this.btnCreate_Account.Size = new System.Drawing.Size(99, 48);
             this.btnCreate_Account.TabIndex = 5;
@@ -77,7 +79,7 @@ namespace Arkansas_Armory
             // cboAccount
             // 
             this.cboAccount.FormattingEnabled = true;
-            this.cboAccount.Location = new System.Drawing.Point(245, 80);
+            this.cboAccount.Location = new System.Drawing.Point(196, 86);
             this.cboAccount.Name = "cboAccount";
             this.cboAccount.Size = new System.Drawing.Size(345, 21);
             this.cboAccount.TabIndex = 6;
@@ -85,28 +87,28 @@ namespace Arkansas_Armory
             // 
             // txtFirst_Name
             // 
-            this.txtFirst_Name.Location = new System.Drawing.Point(245, 139);
+            this.txtFirst_Name.Location = new System.Drawing.Point(196, 145);
             this.txtFirst_Name.Name = "txtFirst_Name";
             this.txtFirst_Name.Size = new System.Drawing.Size(345, 20);
             this.txtFirst_Name.TabIndex = 7;
             // 
             // txtLast_Name
             // 
-            this.txtLast_Name.Location = new System.Drawing.Point(245, 203);
+            this.txtLast_Name.Location = new System.Drawing.Point(196, 209);
             this.txtLast_Name.Name = "txtLast_Name";
             this.txtLast_Name.Size = new System.Drawing.Size(345, 20);
             this.txtLast_Name.TabIndex = 8;
             // 
             // txtEmail
             // 
-            this.txtEmail.Location = new System.Drawing.Point(245, 271);
+            this.txtEmail.Location = new System.Drawing.Point(196, 277);
             this.txtEmail.Name = "txtEmail";
             this.txtEmail.Size = new System.Drawing.Size(345, 20);
             this.txtEmail.TabIndex = 9;
             // 
             // txtPassword
             // 
-            this.txtPassword.Location = new System.Drawing.Point(245, 332);
+            this.txtPassword.Location = new System.Drawing.Point(196, 350);
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.Size = new System.Drawing.Size(345, 20);
             this.txtPassword.TabIndex = 10;
@@ -116,7 +118,7 @@ namespace Arkansas_Armory
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.label3.Font = new System.Drawing.Font("HelveticaNeueforSAS", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(65, 203);
+            this.label3.Location = new System.Drawing.Point(16, 209);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(152, 34);
             this.label3.TabIndex = 11;
@@ -127,7 +129,7 @@ namespace Arkansas_Armory
             this.label4.AutoSize = true;
             this.label4.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.label4.Font = new System.Drawing.Font("HelveticaNeueforSAS", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(65, 271);
+            this.label4.Location = new System.Drawing.Point(16, 277);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(85, 34);
             this.label4.TabIndex = 12;
@@ -138,33 +140,43 @@ namespace Arkansas_Armory
             this.label5.AutoSize = true;
             this.label5.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.label5.Font = new System.Drawing.Font("HelveticaNeueforSAS", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(65, 332);
+            this.label5.Location = new System.Drawing.Point(16, 338);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(140, 34);
             this.label5.TabIndex = 13;
             this.label5.Text = "Password";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.btnCreate_Account);
+            this.panel1.Controls.Add(this.txtPassword);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.cboAccount);
+            this.panel1.Controls.Add(this.txtFirst_Name);
+            this.panel1.Controls.Add(this.txtEmail);
+            this.panel1.Controls.Add(this.txtLast_Name);
+            this.panel1.Location = new System.Drawing.Point(789, 78);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(749, 621);
+            this.panel1.TabIndex = 14;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkRed;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.txtPassword);
-            this.Controls.Add(this.txtEmail);
-            this.Controls.Add(this.txtLast_Name);
-            this.Controls.Add(this.txtFirst_Name);
-            this.Controls.Add(this.cboAccount);
-            this.Controls.Add(this.btnCreate_Account);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(1412, 711);
+            this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -181,6 +193,7 @@ namespace Arkansas_Armory
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
