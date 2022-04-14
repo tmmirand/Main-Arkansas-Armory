500e29aa5b22a37f050715f3680a94a665e4671a
tmmirand <tmmirand@uark.edu>
2022-04-14 11:53:00 -0500

    Deleted code for create account

--- Arkansas Armory/Form1.cs
+++ Arkansas Armory/Form1.cs
@@ -46,13 +46,12 @@ namespace Arkansas_Armory
                 connection = new SqlConnection(connectionstring);
                 connection.Open();
                 int answer;
-                string sql = "INSERT INTO dbo.Customer(Username,Password,FirstName,LastName,ShippingStreetAddress,ShippingCity,ShippingZipcode,ShippingState,BillingStreetAddress,BillingCity,BillingZipcode,BillingState,DoB,DriversLicenseNumber,ConcealCarryStatus,CreditCardNumber,CryptoType,CryptoWalletAddress,Email) VALUES (@User, @Pass, @Fname, @Lname, @ShipStreet, @ShipCity, @ShipZip, @ShipState, @BillStreet, @BillCity, " +
-                    "@BillZip, @BillState, @DoB, @DLNum, @CCStatus, @CCNUmber, @CryptoType, @CryptoWallet,@Email)";
+                string sql = "INSERT INTO dbo.Customer(Password,FirstName,LastName,ShippingStreetAddress,ShippingCity,ShippingZipcode,ShippingState,BillingStreetAddress,BillingCity,BillingZipcode,BillingState,DoB,DriversLicenseNumber,ConcealCarryStatus,CreditCardNumber,CryptoType,CryptoWalletAddress,Email) VALUES (@User, @Pass, @Fname, @Lname, @ShipStreet, @ShipCity, @ShipZip, @ShipState, @BillStreet, @BillCity, " +
+                    "@BillZip, @BillState, @DoB, @DLNum, @CCStatus, @CCNUmber,@Email)";
                 command = new SqlCommand(sql, connection);
                 command.Parameters.AddWithValue("@Fname", txtFirst_Name.Text);
                 command.Parameters.AddWithValue("@Lname", txtLast_Name.Text);
                 command.Parameters.AddWithValue("@Pass", txtPassword.Text);
-                command.Parameters.AddWithValue("@User", txtUsername.Text);
                 command.Parameters.AddWithValue("@ShipStreet", txtShippingStreetAddress.Text);
                 command.Parameters.AddWithValue("@ShipCity", txtShippingCity.Text);
                 command.Parameters.AddWithValue("@ShipZip", txtShippingZip.Text);
@@ -65,9 +64,6 @@ namespace Arkansas_Armory
                 command.Parameters.AddWithValue("@DLNum", txtDriverLiscenceNum.Text);
                 command.Parameters.AddWithValue("@CCStatus", txtConcealedCarry.Text);
                 command.Parameters.AddWithValue("@CCNumber", txtCreditCardNum.Text);
-              
-                command.Parameters.AddWithValue("@CryptoType", txtCryptoType.Text);
-                command.Parameters.AddWithValue("@CryptoWallet", txtBillingCity.Text);
                 command.Parameters.AddWithValue("@Email", txtEmail.Text);
 
 
